2667c4f98f3ae3c1517553dda450a83b7075d0f3
l3g10n <ejise45@gmail.com>
2022-11-27 23:12:10 +0100

    added logging

--- src/server.rs
+++ src/server.rs
@@ -1,11 +1,13 @@
 use actix_web::{ HttpServer, App};
+use actix_web::middleware::Logger;
 use std::env;
+use env_logger::Env;
 use actix_files::Files;
 
 #[actix_web::main]
 pub async fn server() -> std::io::Result<()> {
     std::env::set_var("RUST_LOG", "actix_web=info");
-    env_logger::init();
+    env_logger::init_from_env(Env::default().default_filter_or("info"));
     // Get the port number to listen on.
     let port = env::var("PORT")
         .unwrap_or_else(|_| "8000".to_string())
@@ -15,7 +17,9 @@ pub async fn server() -> std::io::Result<()> {
     // Start a server, configuring the resources to serve.
     HttpServer::new(|| {
         App::new()
+            .wrap(Logger::default())
             .service(Files::new("/", "./static/root").index_file("index.html"))
+            .wrap(Logger::new("%a %t %r %s %b %P %s %D %{Referer}i %{User-Agent}i %T"))
     })
     .bind(("0.0.0.0", port))
     .expect("Can not bind to port 8000")
